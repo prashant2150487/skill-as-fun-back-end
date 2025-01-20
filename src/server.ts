43e2a95714614adaee4f9f89e8d5504336b7636d
Prashant Sachan <p-prashantsachan@goodearth.in>
2025-01-20 23:21:51 +0530

    delete whoel code

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-import { configEnv } from "./config/app/env.config";
-
-const PORT = configEnv.port || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
