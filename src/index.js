49d3d9fd4b416a2a407f3e23cba089d0c4e4978c
Prashant Sachan <p-prashantsachan@goodearth.in>
2025-01-22 00:13:51 +0530

    add build

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ import express from "express";
 import connectDB from "./config/database.js";
 import authRoutes from "./routes/authRoutes.js";
 
+// Initialize the app
 const app = express();
-
+// Database connection
 connectDB();
-
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
-const PORT = process.env.PORT || 3000;
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Something went wrong!" });
 });
+// Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
