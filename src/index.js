f8dcfbf599bd84b4b60ec60c56683b71fc45f0a8
Prashant Sachan <p-prashantsachan@goodearth.in>
2025-01-22 00:26:59 +0530

    remove build folder

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const PORT = process.env.PORT || 3000;
 app.use("/api", (req, res) => {
   res.send("Welcome to the API");
 });
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
