83a1ed671e20fec03b586afe7f1449dfc187efa4
Prashant Sachan <your-email@example.com>
2025-07-05 10:51:51 +0530

    feat: update Question schema to require 4 options and adjust correctIndex enum

--- src/models/question.js
+++ src/models/question.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 const QuestionSchema = new mongoose.Schema({
   quizId: { type: mongoose.Schema.Types.ObjectId, ref: "Quiz", required: true },
   text: { type: String, required: true },
-  options: { type: [String], length: 2,validate: v => v.length === 2, required: true },
-  correctIndex: { type: Number, enum: [0, 1], required: true },
+  options: {
+    type: [String],
+    length: 4,
+    validate: (v) => v.length === 4,
+    required: true,
+  },
+  correctIndex: { type: Number, enum: [1, 2, 3, 4], required: true },
 });
 export default mongoose.model("Question", QuestionSchema);
